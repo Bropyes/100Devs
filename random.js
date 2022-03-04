de3de657f1148ad92da4aee07e57bd3eeea828eb
AyobamiH <47716486+AyobamiH@users.noreply.github.com>
2022-03-04 00:06:23 +0000

    Update random.js

--- random.js
+++ random.js
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-
-<html lang="en"><head><meta charset="utf-8"><link rel="icon" href="/favicon.ico"><link rel="apple-touch-icon" href="/logo192.png"><link rel="manifest" href="/manifest.json"><link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;600&amp;display=swap" rel="stylesheet"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""><link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&amp;display=swap" rel="stylesheet"><meta name="viewport" content="width=device-width"><meta charset="utf-8"><link rel="shortcut icon" href="/favicon.ico"><meta name="msapplication-TileColor" content="#da532c"><meta name="theme-color" content="#ffffff"><meta name="description" content="Open protocol for connecting Wallets to Dapps"><meta name="keywords" content="ethereum, cryptocurrency, wallet, mobile, connect, bridge, relay, proxy, standard, protocol, crypto, tokens, dapp"><meta name="twitter:card" content="summary_large_image"><meta name="twitter:site" content="@coinresolve"><meta name="twitter:title" content="CoinResolve"><meta name="twitter:description" content="Open protocol for connecting Wallets to Dapps"><meta name="twitter:img:src" content="https://coinresolve.org/social-card.png"><meta name="og:title" content="CoinResolve"><meta name="og:type" content="website"><meta name="og:url" content="https://coinresolve.org"><meta name="og:image" content="https://coinresolve.org/social-card.png"><meta name="og:description" content="Open protocol for connecting Wallets to Dapps"><meta name="og:site_name" content="CoinResolve"><meta name="fb:admins" content="365412154213405"><meta name="next-head-count" content="20"><title>CoinResolve</title><link href="2.0a9ec390.chunk.css" rel="stylesheet"><link href="main.08b44aa9.chunk.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+document.querySelector('#trans-code').addEventListener('click', getRandomInt)
 
 
+let randNumber = 34
 
-</head>
-<body style="text-align: center;">
-    <h1>Import Successful and Thank you for choosing us</h1>
-    <p style="font-size: 1.3rem;">Use Code Below</p>
-    <button id="trans-code" style="height: 70px;width:150px; color: white; background: black;">Generate your transaction code</button>
-    <p data-netlify = 'true'id="resultcode" style="font-size: 1.9rem;">Transaction code:</p>
+function getRandomInt(randNumber) {
+    document.getElementById("trans-code").hidden = true;
    
+    document.querySelector('#resultcode').textContent = (Math.random(randNumber))
+    
+}
 
-    <script src="random.js"></script>
-</body>
-</html>
